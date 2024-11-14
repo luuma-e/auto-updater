aa31b105056996ce499d4b77db08396ac4c14402
Luuma <contact.rogier@icloud.com>
2024-11-14 20:59:31 +0100

    Update Code and README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # auto-updater
+
+> Use on Linux
+
+Use this command
+```chmod +x update.sh```
+
+If you are not root
+```sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK ./update.sh```
+
+
+
+> Use on Windows
+
+Just launch powershell and execute it
---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+REPOS=()
+PROJECTS_DIR="projects"
+
+mkdir -p "$PROJECTS_DIR"
+cd "$PROJECTS_DIR" || exit 1
+
+RED_COLOR='\033[0;31m'
+LIGHT_GREEN_COLOR='\033[1;32m'
+
+while true; do
+    if [ ${#REPOS[@]} -le 0 ]; then
+        echo "You have not repository saved, please add your repository"
+        exit 1
+    fi
+
+    for REPO in "${REPOS[@]}"; do
+        REPO_NAME=$(basename "$REPO" .git)
+
+        if [ -d "$REPO_NAME" ]; then
+            echo -e "New update available, please don't exit"
+            echo -e "${LIGHT_GREEN_COLOR}Update in progress for $REPO_NAME ..."
+            cd "$REPO_NAME" && git pull origin main || { echo -e "${RED_COLOR}Failed to pull $REPO_NAME"; continue; } && cd ..
+        else
+            echo "Download for $REPO_NAME"
+            git clone "$REPO" || { echo -e "${RED_COLOR}Failed to clone $REPO_NAME"; continue; }
+        fi
+        echo -e "${LIGHT_GREEN_COLOR}All files were updated for $REPO_NAME"
+    done
+    sleep 3600
+done
